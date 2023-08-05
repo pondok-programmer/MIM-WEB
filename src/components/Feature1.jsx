94240b546526b731b19109004399dc19ab20fdb3
edwards <zedwar19@gmail.com>
2023-08-05 14:46:52 +0700

    update

--- src/components/Feature1.jsx
+++ src/components/Feature1.jsx
@@ -1,7 +1,25 @@
+import imgSmartphone from '../assets/imgs'
 
 const Feature1 = () => {
   return (
-    <div>Feature1</div>
+    <section className="">
+      <header className="text-center">
+        <h1 className="text-[26px] md:text-[28px] font-bold mb-2">Fitur - Fitur</h1>
+      </header> 
+      <main>
+        <h2>Fitur Utama</h2>
+        <article>
+          <section>
+            <h1>MIM Standar</h1>
+            <h1>MIM Doa Dalam Quran</h1>
+            <h1>MIM Bacaan Sholat</h1>
+          </section>
+          <section>
+            <img src={imgSmartphone} alt="" />
+          </section>
+        </article>
+      </main>
+    </section>
   )
 }
 
