87d134f68e15b1b9fb9d448b6866514048dbcb9d
edwards <zedwar19@gmail.com>
2023-08-16 17:01:41 +0700

    adding footer component

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -4,7 +4,7 @@ import example from '../assets/imgs/SVG1.svg'
 
 const Download = () => {
   return (
-    <section className='relative w-[93%] h-[40vh] float-right rounded-tl-3xl px-5 rounded-bl-3xl my-20 overflow-hidden bg-gradient-to-l from-[#FAEAB1] to-[#FFFFFF] shadow-[inset_0px_0px_20px_rgba(0,0,0,0.6)] flex justify-evenly items-center'>
+    <section className='relative w-[93%] h-[40vh] float-right rounded-tl-3xl px-5 rounded-bl-3xl mt-20 overflow-hidden bg-gradient-to-l from-[#FAEAB1] to-[#FFFFFF] shadow-[inset_0px_0px_20px_rgba(0,0,0,0.6)] flex justify-evenly items-center'>
       <header className='self-end w-[40%] bottom-0 mt-10'>
         <img src={example} alt="" className='w-[110px] object-cover'/>
       </header>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,41 +9,42 @@ import { BiLogoFacebookCircle, BiLogoGmail, BiLogoInstagram, BiLogoWhatsapp, BiL
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col justify-end gap-y-3 w-full h-[60vh] bg-gradient-to-t from-[#FAEAB1] from-20% to-[#FFFFFF] to-80% text-[#AD7B3E]">
-      <div>
-        <section className="flex items-center">
-          <header className='flex-[1] mx-4'><h1 className='text-bold text-[18px]'>Partnership</h1></header>
-          <main className="flex flex-[3] h-16 overflow-x-scroll scrollbar-hide">
-            <img src={brandSidaq} alt="Logo Sidaq" />
-            <img src={brandIsmuhuyahya} alt="Logo IsmuhuYahya" />
-            <img src={brandMPP} alt="Logo Masjid Pemuda Peradaban" />
-            <img src={brandPIT} alt="Logo Pondok IT" />
-            <img src={brandSIB} alt="Logo SIB" />
+    <footer className="flex flex-col md:flex-row justify-end gap-y-3 md:gap-10 lg:gap-0 w-full h-[55vh] bg-gradient-to-t from-[#FAEAB1] from-30% to-[#FFFFFF] to-80% text-[#AD7B3E] md:relative md:justify-center">
+      <div className='md-flex-[1] lg:flex-[2] md:w-fit md:h-fit md:mb-20 md:self-end'>
+        <section className="flex items-center md:flex-col lg:flex-row md:w-fit md:h-[30vh] lg:pl-10">
+          <header className='flex-[1] mx-4 lg:-mt-20'><h1 className='text-bold text-[18px] md:text-[24px]'>Partnership</h1></header>
+          <main className="flex flex-[3] lg:flex-row lg:h-[210px] lg:mt-20 lg:-ml-40 md:flex-wrap h-16 md:w-[300px] lg:w-[450px] overflow-x-scroll md:overflow-y-scroll scrollbar-hide">
+            <img src={brandSidaq} alt="Logo Sidaq" className='md:w-[170px] md:h-[60px] md:mx-16 lg:mx-60 lg:mb-1'/>
+            <img src={brandIsmuhuyahya} alt="Logo IsmuhuYahya" className='md:w-[90px] md:h-[90px] md:ml-14 lg:-ml-5' />
+            <img src={brandMPP} alt="Logo Masjid Pemuda Peradaban" className='md:w-[90px] md:h-[90px]' />
+            <img src={brandPIT} alt="Logo Pondok IT" className='md:w-[120px] md:h-[80px] md:ml-7 lg:ml-0' />
+            <img src={brandSIB} alt="Logo SIB" className='md:w-[120px] md:h-[80px]' />
           </main>
         </section>
       </div>
-      <div className='flex flex-col items-center'>
+      <div className='flex flex-col lg:flex-row md:self-center md:gap-3 lg:gap-20 items-center md:flex-[3] md:mt-20 md:mr-10'>
       <section className='text-center'>
-        <header>
-          <h1>Kantor</h1>
+        <header className='md:mb-10'>
+          <h1 className='md:text-[24px] '>Kantor</h1>
         </header>
-        <main>
+        <main className='md:text-[19px]'>
           <p>Desa Tirtohargo dsn Kalangan, Rt/Rw 01/00, Gegunung, Tirtohargo, Kec. Kretek, Kabupaten Bantul, Daerah Istimewa Yogyakarta 55772</p>
         </main>
       </section>
-      <section className='text-center my-5'>
+      <section className='text-center mt-4 mb-4'>
         <header>
-        <h1>Hubungi Kami</h1></header>
-        <main className='flex gap-5'>
-          <span><BiLogoInstagram  className='text-[30px]'/></span>
-          <span><BiLogoYoutube  className='text-[30px]'/></span>
-          <span><BiLogoFacebookCircle  className='text-[30px]'/></span>
-          <span><BiLogoGmail  className='text-[30px]'/></span>
-          <span><BiLogoWhatsapp  className='text-[30px]'/></span>
+        <h1 className='md:text-[24px] '>Hubungi Kami</h1></header>
+        <main className='flex gap-5 md:gap-x-7 lg:flex-wrap mt-4 md:w-[200px] md:justify-center'>
+          <span><BiLogoInstagram  className='text-[30px] md:text-[39px]'/></span>
+          <span><BiLogoYoutube  className='text-[30px] md:text-[39px]'/></span>
+          <span><BiLogoFacebookCircle  className='text-[30px] md:text-[39px]'/></span>
+          <span><BiLogoGmail  className='text-[30px] md:text-[39px]'/></span>
+          <span><BiLogoWhatsapp  className='text-[30px] md:text-[39px]'/></span>
         </main>
       </section>
       </div>
-      <span className='text-center font-[600]'>
+      <span className='text-center text-[18px] md:text-[20px] font-[600] mb-5 md:absolute md:bottom-0'>
+        <hr className='mx-auto w-80 lg:w-[60vw] mb-5 bg-[#AD7B3E]' />
         &copy; 2023 MIM | All Rights Reserved.
       </span>
     </footer>
